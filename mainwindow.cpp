e7ded0776130c08c2016b76dae5899160153a8fa
nuaazzs <zh_zsh@163.com>
2018-12-21 16:31:10 +0800

    Revert "from github.com"
    This reverts commit fef7e139e8e55957ff8a3fc1ac849476e33baf24.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -12,7 +12,6 @@ MainWindow::MainWindow(QWidget *parent) :
     lb1->setText("18.12.21 16:03 add this from qt 1507");
     lb1->setGeometry(50,100,300,40);
     lb1->show();
-    //18.12.21 16:09 add this from github.com
 }
 
 MainWindow::~MainWindow()
