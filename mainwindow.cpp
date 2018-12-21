b6eeb136002afd82b6181545e06891a02a602905
nuaazzs <zh_zsh@163.com>
2018-12-21 16:15:45 +0800

    change the code and create a new branch
    change the code and create a new branch 18.12.21 16:15

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -13,6 +13,11 @@ MainWindow::MainWindow(QWidget *parent) :
     lb1->setGeometry(50,100,300,40);
     lb1->show();
     //18.12.21 16:09 add this from github.com
+
+    QLabel* lb2 = new QLabel(ui->centralWidget);
+    lb2->setText("18.12.21 16:14 want to create a new branch");
+    lb2->setGeometry(50,200,300,40);
+    lb2->show();
 }
 
 MainWindow::~MainWindow()
